93d214b864d9e1d3026424a5fc8d5a2505c509f7
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2022-06-21 21:23:25 +0100

    My first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// METHOD A
+
+document.querySelector('.box').innerHTML = `
+<h1> NAME : EZIKE NKEMDILI JANE </h1>
+<h1> HEIGHT : 165cm </h1>
+<h1> COUNTRY : NIGERIA </h1>
+`
+
+// METHOD B
+document.write('NAME: EZIKE NKEMDILI JANE', "<br> ") 
+document.write('HEIGHT: 165cm' , "<br> ")   
+document.write('COUNTRY: NIGERIA')
